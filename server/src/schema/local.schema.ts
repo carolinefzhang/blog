2a714dfdb8c110dfd19fb634511c87b41161b3f7
Caroline Zhang <caroline.fzhang@gmail.com>
2025-11-08 11:54:44 +1100

    update syntax

--- server/src/schema/local.schema.ts
+++ server/src/schema/local.schema.ts
@@ -1,6 +1,6 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./index.js";
-import resolvers from "../resolvers.js";
+import resolvers from "../resolvers/index.js";
 
 const localSchema = makeExecutableSchema({
   typeDefs,
