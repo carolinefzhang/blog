8aa60a8c8f8f1fa09ef7dc89bc26a959f6a570fd
Caroline Zhang <caroline.fzhang@gmail.com>
2025-11-10 12:39:46 +1100

    update graphql uri

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:5050/graphql',
+    uri: import.meta.env.VITE_GRAPHQL_URI || 'http://localhost:5050/graphql',
   }),
   cache: new InMemoryCache(),
 });
